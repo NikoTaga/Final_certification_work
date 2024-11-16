19f15416005622983196319e30b6b03327fb5905
NikoTaga <kraffchenko@mail.ru>
2024-11-16 15:26:33 +0300

    добавлены модели

--- data_processing.py
+++ data_processing.py
@@ -1,11 +1,30 @@
-def process(area, par1=1):
+import pickle
+
+from numpy import round
+
+
+def calc_cost(area):
     try:
         area = float(area)
     except:
         area = 0
 
-    res = area * par1
-    return res
+    model = _load_model("models/rf.pkl")
+    scaler_x = _load_model("models/scaler_x.pkl")
+    scaler_y = _load_model("models/scaler_y.pkl")
+
+    params = scaler_x.transform([[area]])
+    y_pred = model.predict(params)
+    result = scaler_y.inverse_transform([y_pred])
+
+    return round(result[0][0], 2)
+
+
+def _load_model(path):
+    with open(path, "rb") as f:
+        model = pickle.load(f)
+    return model
+
 
 
 
